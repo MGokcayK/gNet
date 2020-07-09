6d7b2afe29a3b5ef6f2f4476897e4e2f04f3b176
MGokcayK <mgokcaykdev@gmail.com>
2020-07-09 15:59:52 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gNet", 
+    version="0.1.0",
+    author="Mehmet Gökçay Kabataş",
+    author_email="mgokcaykdev@gmail.com",
+    description="A mini Deep Learning Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MGokcayK/gNet",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
